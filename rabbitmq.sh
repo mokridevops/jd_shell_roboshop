b6cb865e2a200d1a8a2bed35b69b34a5f55a8acd
mokridevops <mokridevops@gmail.com>
2026-09-15 14:34:05 -0500

    added redis installation script

---
+++ rabbitmq.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+LOGS_FOLDER="/var/log/roboshop"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+
+sudo mkdir -p $LOGS_FOLDER
+sudo chown -R ec2-user:ec2-user $LOGS_FOLDER
+sudo chmod -R 755 $LOGS_FOLDER
+
+TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+trap 'echo "error at $LINENO", command: $BASH_COMMAND" ' ERR
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$TIMESTAMP [ERROR]....$R Please run this script with root access $N" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+VALIDATE()
+{
+    if [ $1 -ne 0 ]; then
+        echo -e "TIMESTAMP [ERROR] .... $2 ....$R FAILURE $N" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo -e "TIMESTAMP [INFO] ....$2 .....$G SUCCESS $N" | tee -a $LOGS_FILE
+    fi 
+}
+
+
+cp rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo
+VALIDATE $? "Copying RabbitMq Repo"
+
+dnf install rabbitmq-server -y &>> $LOGS_FILE
+VALIDATE $? "Installing rabbitmq-server"
+
+systemctl enable rabbitmq-server &>> $LOGS_FILE
+systemctl start rabbitmq-server  &>> $LOGS_FILE
+VALIDATE $? "enabling and starting rabbitmq-server"
+
+rabbitmqctl add_user roboshop roboshop123 &>> $LOGS_FILE
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"  &>> $LOGS_FILE
+
+
