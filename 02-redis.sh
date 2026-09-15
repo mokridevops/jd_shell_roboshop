9b75a2280c41ff6a4e93c9e54218c5db7688cabd
mokridevops <mokridevops@gmail.com>
2026-09-15 13:54:24 -0500

    added redis installation script

--- 02-redis.sh
+++ 02-redis.sh
@@ -33,20 +33,19 @@ VALIDATE()
 }
 
 
-dnf module disable redis -y >> $LOGS_FILE
-dnf module enable redis:7 -y >> $LOGS_FILE
-VALIDATE $? "Disable and Enable redis"
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "Copying Mongo Repo"
 
+dnf install mongodb-org -y &>> $LOGS_FILE
+VALIDATE $? "Installing MongoDB"
 
-dnf install redis -y  >> $LOGS_FILE
-VALIDATE $? "Installing redis"
+systemctl enable --now mongod &>> $LOGS_FILE
+VALIDATE $? "enabling and starting mongod"
 
+sed 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>> $LOGS_FILE
+VALIDATE $? "Allowing remote connections to MongoDB"
 
+systemctl restart mongod
+VALIDATE $? "restarting mongodb"
 
-sed -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf &>> $LOGS_FILE
-VALIDATE $? "Allowing remote connections to redis"
-
-systemctl enable redis >> $LOGS_FILE
-systemctl start redis >> $LOGS_FILE
-VALIDATE $? "enabling and restarting redis"
 
