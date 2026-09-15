f03f7b06b32f2d7cb7d9f90cd41eb554b80730db
mokridevops <mokridevops@gmail.com>
2026-09-15 15:45:33 -0500

    added redis installation script

--- 05-catalogue.sh
+++ 05-catalogue.sh
@@ -27,10 +27,10 @@ fi
 VALIDATE()
 {
     if [ $1 -ne 0 ]; then
-        echo -e "TIMESTAMP [ERROR] .... $2 ....$R FAILURE $N" | tee -a $LOGS_FILE
+        echo -e "$TIMESTAMP [ERROR] .... $2 ....$R FAILURE $N" | tee -a $LOGS_FILE
         exit 1
     else
-        echo -e "TIMESTAMP [INFO] ....$2 .....$G SUCCESS $N" | tee -a $LOGS_FILE
+        echo -e "$TIMESTAMP [INFO] ....$2 .....$G SUCCESS $N" | tee -a $LOGS_FILE
     fi 
 }
 
@@ -44,7 +44,7 @@ VALIDATE $? "enabling nodejs 20"
 dnf install nodejs -y &>> $LOGS_FILE
 VALIDATE $? "installing nodejs"
 
-id roboshop &>> $LOGS_FILE
+id roboshop
 if [ $? -ne 0]; then
 useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>> $LOGS_FILE
 VALIDATE $? "creating roboshop system user"
